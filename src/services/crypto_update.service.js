90cfff4a390fd9d70ceb90e59578478bbb23c169
Karthikeya <yelisettikarthik0@gmail.com>
2024-04-13 16:45:35 +0530

    fix db update routine for efficiency

--- src/services/crypto_update.service.js
+++ src/services/crypto_update.service.js
@@ -13,29 +13,59 @@ const options = {
     },
 }
 
-async function updateCoins() {
+const updateCoins = async () => {
     logger.info('Updating coins...')
-    const response = await axios.request(options)
-    const coins = response.data
-    let num_coins = coins.length
-    let num_coins_db = await Coin.countDocuments()
-    coins.forEach(async (coin) => {
-        try {
-            await Coin.findOneAndUpdate({ id: coin.id }, coin, { upsert: true })
-        } catch (error) {
-            logger.error(error)
+    const start = new Date()
+
+    try {
+        const response = await axios.request(options)
+        const coins = response.data
+        const coins_in_db = await Coin.find({}).lean()
+        let num_coins = coins.length
+        let num_coins_db = await Coin.countDocuments()
+
+        // Documents to update
+        const coins_to_insert = []
+        const coins_to_delete = []
+
+        coins_in_db.forEach((coin) => {
+            const found = coins.find((c) => c.id === coin.id)
+            if (!found) {
+                coins_to_delete.push(coin)
+            }
+        })
+
+        coins.forEach((coin) => {
+            const found = coins_in_db.find((c) => c.id === coin.id)
+            if (!found) {
+                coins_to_insert.push(coin)
+            }
+        })
+
+        // Insert new coins
+        if (coins_to_insert.length > 0) {
+            await Coin.insertMany(coins_to_insert)
         }
-    })
 
-    // Remove coins that are not in the response
-    await Coin.deleteMany({ id: { $nin: coins.map((coin) => coin.id) } })
+        // Delete coins
+        if (coins_to_delete.length > 0) {
+            const coins_to_delete_ids = coins_to_delete.map((coin) => coin._id)
+            await Coin.deleteMany({ _id: { $in: coins_to_delete_ids } })
+        }
 
-    let updated_coins = await Coin.countDocuments()
-    logger.info(
-        `Synchronized ${num_coins} coins in the database (${num_coins_db} -> ${updated_coins})`
-    )
+        let updated_coins = await Coin.countDocuments()
 
-    return
+        const end = new Date()
+        const duration_str = (end - start).toLocaleString()
+        logger.info(
+            `Synchronized ${num_coins} coins in the database (${num_coins_db} -> ${updated_coins}) [${duration_str} ms]`
+        )
+    } catch (error) {
+        const end = new Date()
+        const duration_str = (end - start).toLocaleString()
+        logger.error(`Error updating coins [${duration_str} ms]`)
+        logger.error(error)
+    }
 }
 
 module.exports = {
